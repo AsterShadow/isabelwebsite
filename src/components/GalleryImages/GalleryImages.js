a7222e179dff109c13c0e81c52576f463ba4f84b
Xzandro <xzanvas@gmail.com>
2024-07-17 12:38:23 -0400

    added sanity to project

--- src/components/GalleryImages/GalleryImages.js
+++ src/components/GalleryImages/GalleryImages.js
@@ -1,26 +1,103 @@
+"use client";
 import Images from "@/assets/images";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const GalleryImages = () => {
+  const router = useRouter();
+
   const placeholders = [
-    Images.landing1,
-    Images.landing2,
-    Images.landing3,
-    Images.landing4,
-    Images.landing5,
-    Images.landing1,
-    Images.landing2,
-    Images.landing3,
-    Images.landing4,
-    Images.landing5,
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing1,
+      title: "Douceur et volupté",
+      price: 150,
+      createdAt: 2023,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "calm-lake",
+      image: Images.landing2,
+      title: "Calm Lake",
+      price: 320,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "dream-of-india",
+      image: Images.landing3,
+      title: "Dream of India",
+      price: 500,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing4,
+      title: "Douceur et volupté",
+      price: 450,
+      createdAt: 2022,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing1,
+      title: "Douceur et volupté",
+      price: 150,
+      createdAt: 2023,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "calm-lake",
+      image: Images.landing2,
+      title: "Calm Lake",
+      price: 320,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "dream-of-india",
+      image: Images.landing3,
+      title: "Dream of India",
+      price: 500,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing4,
+      title: "Douceur et volupté",
+      price: 450,
+      createdAt: 2022,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
   ];
   return (
-    <div className="full grid grid-cols-1 md:grid-cols-2 gap-4 px-2">
+    <div className="full columns-2 2xl:columns-3 px-2">
       {placeholders.map((image, index) => (
-        <div key={index} className="max-w-[500px]">
-          <Image src={image} alt="gallery painting" />
-        </div>
+        <Link href={`/product/${image.slug}`}>
+          <div key={index} className="w-full h-auto object-cover py-2">
+            <Image src={image.image} alt="gallery painting" />
+          </div>
+        </Link>
       ))}
     </div>
   );
--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,9 +1,92 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
 
 const Product = () => {
-  return (
-    <div></div>
-  )
-}
+  const router = useRouter();
+  const { slug } = router.query;
+  const placeholders = [
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing1,
+      title: "Douceur et volupté",
+      price: 150,
+      createdAt: 2023,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "calm-lake",
+      image: Images.landing2,
+      title: "Calm Lake",
+      price: 320,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "dream-of-india",
+      image: Images.landing3,
+      title: "Dream of India",
+      price: 500,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing4,
+      title: "Douceur et volupté",
+      price: 450,
+      createdAt: 2022,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing1,
+      title: "Douceur et volupté",
+      price: 150,
+      createdAt: 2023,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "calm-lake",
+      image: Images.landing2,
+      title: "Calm Lake",
+      price: 320,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "vendue",
+      medium: "acrylique",
+    },
+    {
+      slug: "dream-of-india",
+      image: Images.landing3,
+      title: "Dream of India",
+      price: 500,
+      createdAt: 2024,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+    {
+      slug: "douceur-et-volupte",
+      image: Images.landing4,
+      title: "Douceur et volupté",
+      price: 450,
+      createdAt: 2022,
+      dimensions: "20 x 28 pouces",
+      status: "disponible",
+      medium: "acrylique",
+    },
+  ];
+  return <div></div>;
+};
 
 export default Product;
