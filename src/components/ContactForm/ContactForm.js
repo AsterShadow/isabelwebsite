f40804a85283090b6fc2d18c2869762cfd409402
Xzandro <xzanvas@gmail.com>
2024-07-30 13:46:39 -0400

    form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,19 +1,34 @@
 "use client";
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function ContactForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   console.log(errors);
-  
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Nom" {...register("Nom", {required: true})} />
-      <input type="email" placeholder="Email" {...register("Email", {required: true})} />
-      <textarea {...register("Message", {required: true})} />
 
-      <input type="submit" />
-    </form>
+  return (
+    <div className="w-full">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full items-center justify-center">
+        <input
+          type="text"
+          placeholder="Nom"
+          {...register("Nom", { required: true })}
+          className=""
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          {...register("Email", { required: true })}
+        />
+        <textarea {...register("Message", { required: true })} />
+
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
