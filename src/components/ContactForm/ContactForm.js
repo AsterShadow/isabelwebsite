8b4cf54b0b413f3739606a9e03a8bc36e070b76a
AsterShadow <xzanvas@gmail.com>
2024-07-30 15:31:27 -0400

    form style

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -12,22 +12,43 @@ export default function ContactForm() {
   console.log(errors);
 
   return (
-    <div className="w-full">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full items-center justify-center">
-        <input
-          type="text"
-          placeholder="Nom"
-          {...register("Nom", { required: true })}
-          className=""
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          {...register("Email", { required: true })}
-        />
-        <textarea {...register("Message", { required: true })} />
+    <div className="w-full max-w-[800px]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-full items-center justify-center"
+      >
+        <div className="w-full">
+          <p>Nom</p>
+          <input
+            type="text"
+            placeholder="Nom"
+            {...register("Nom", { required: true })}
+            className="rounded-md border-2 border-third bg-light p-2 w-full text-dark"
+          />
+        </div>
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            type="email"
+            placeholder="Email"
+            {...register("Email", { required: true })}
+            className="rounded-md border-2 border-third p-2 w-full text-dark"
+          />
+        </div>
+        <div className="w-full">
+          <p>Message</p>
+          <textarea
+            {...register("Message", { required: true })}
+            placeholder="Message"
+            className="rounded-md border-2 border-third p-2 w-full text-dark"
+          />
+        </div>
 
-        <input type="submit" />
+        <input
+          type="submit"
+          value="Envoyer"
+          className="cursor-pointer text-center text-light text-md drop-shadow-sm rounded-md p-3 w-48 bg-third transition-all hover:bg-info focus:translate-y-px"
+        />
       </form>
     </div>
   );
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,21 @@
 import ContactForm from "@/components/ContactForm/ContactForm";
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Header/Header";
 import LayoutContainer from "@/components/LayoutContainer/LayoutContainer";
 import React from "react";
 
 const contact = () => {
   return (
-    <LayoutContainer>
-      <Navbar />
-      <div className="w-full flex flex-col z-20 items-center h-full py-36">
-        <ContactForm />
-      </div>
-    </LayoutContainer>
+    <>
+      <LayoutContainer>
+        <Navbar />
+        <div className="w-full flex flex-col z-20 items-center h-full py-36">
+          <h2 className="text-2xl md:text-4xl font-headers">Contactez-moi !</h2>
+          <ContactForm />
+        </div>
+      </LayoutContainer>
+      <Footer />
+    </>
   );
 };
 
