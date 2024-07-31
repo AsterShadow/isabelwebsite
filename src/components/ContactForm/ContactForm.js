5dfc017c95ed27a3a04e5ad50e25d1202fc82aa2
AsterShadow <xzanvas@gmail.com>
2024-07-31 12:16:33 -0400

    added emailjs reacthookform and toastify to contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function ContactForm() {
   const {
@@ -8,7 +11,40 @@ export default function ContactForm() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const sendEmail = (params) => {
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        params,
+        {
+          publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+
+          limitRate: {
+            throttle: 5000,
+          },
+        }
+      )
+      .then(
+        () => {
+          toast.success("Email sent successfully!");
+        },
+        (error) => {
+          toast.error("Failed to send email. Please try again.");
+          console.error("FAILED...", error.text);
+        }
+      );
+  };
+  const onSubmit = (data) => {
+    const templateParams = {
+      to_name: "Isabel",
+      from_name: data.name,
+      reply_to: data.email,
+      message: data.message,
+    };
+    sendEmail(templateParams);
+  };
   console.log(errors);
 
   return (
@@ -22,7 +58,7 @@ export default function ContactForm() {
           <input
             type="text"
             placeholder="Nom"
-            {...register("Nom", { required: true })}
+            {...register("name", { required: true })}
             className="rounded-md border-2 border-third bg-light p-2 w-full text-dark"
           />
         </div>
@@ -31,14 +67,14 @@ export default function ContactForm() {
           <input
             type="email"
             placeholder="Email"
-            {...register("Email", { required: true })}
+            {...register("email", { required: true })}
             className="rounded-md border-2 border-third p-2 w-full text-dark"
           />
         </div>
         <div className="w-full">
           <p>Message</p>
           <textarea
-            {...register("Message", { required: true })}
+            {...register("message", { required: true })}
             placeholder="Message"
             className="rounded-md border-2 border-third p-2 w-full text-dark"
           />
@@ -50,6 +86,7 @@ export default function ContactForm() {
           className="cursor-pointer text-center text-light text-md drop-shadow-sm rounded-md p-3 w-48 bg-third transition-all hover:bg-info focus:translate-y-px"
         />
       </form>
+      <ToastContainer />
     </div>
   );
 }
